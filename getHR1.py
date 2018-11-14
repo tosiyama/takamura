a4863be92616363b5c6ae66c9061eb094805c3c8
Toshiaki Yamanouchi <tosiyama@gmail.com>
2018-11-14 09:00:35 +0900

    Tokenをjsonファイルから読み込むようにした

--- getHR1.py
+++ getHR1.py
@@ -1,14 +1,41 @@
 import fitbit
+import json
+import datetime
+import time
+import sys
+
+def loadConfig_TokenFile(C_filename):
+    f2 = open(C_filename, 'r')
+    jsonData2 = json.load(f2)
+    param = {}
+    param['client_id'] = jsonData2['client_id']
+    param['client_secret'] = jsonData2['client_secret']
+    T_filename = jsonData2['token_file']
+
+    f1 = open(T_filename, 'r')
+    jsonData1 = json.load(f1)
+    param['access_token'] = jsonData1['access_token']
+    param['refresh_token'] = jsonData1['refresh_token']
+    param['expires_at'] = jsonData1['expires_at']
+
+    return param
 
 if __name__ == '__main__':
-    client_id = '22D5WJ'
-    client_secret = '82d281cf8d5b8c19720f20c04571d691'
-    access_token =  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMkQ1V0oiLCJzdWIiOiI2WFFDSlMiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJhY3QgcnNldCBybG9jIHJ3ZWkgcmhyIHJudXQgcnBybyByc2xlIiwiZXhwIjoxNTQyMDIzOTUwLCJpYXQiOjE1NDE5OTUxNTB9.hQ493ERXC6o9CtIk96wIGzAf9CZNmGVRCNm5aQdJOrQ'
-    refresh_token = '78018639dd15c4ea6e6fa912b670e98db38d381ca07b0f4317ca463b99db9a8b'
-    
+    # 取得したい心拍数データの日付    
     date = '2018-11-11'
+    
+    config_filename = 'config.json'
+    param = loadConfig_TokenFile(config_filename)
 
-    ac = fitbit.Fitbit(client_id, client_secret, access_token, refresh_token)
+    dt = datetime.datetime.now()
+    expire_dt =  datetime.datetime.fromtimestamp(param['expires_at'])
+    if dt >= expire_dt:
+        s = str(expire_dt)
+        print('トークンの有効期限 {0} を過ぎてしまいました!'.format(s))
+        print('新しいトークンを入手してください!')
+        sys.exit()
+    
+    ac = fitbit.Fitbit(param['client_id'], param['client_secret'], param['access_token'], param['refresh_token'])
 
     # 心拍数を取得(1秒ごと)
     data_sec = ac.intraday_time_series('activities/heart', date, detail_level='1sec') #'1sec', '1min', or '15min'
